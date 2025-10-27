1f2f1b27a9e5f978bed6817fc31c92201c40f9ca
Samriddha11 <155475136+Samriddha11@users.noreply.github.com>
2025-10-27 23:59:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ provider "aws" {
   region = var.region
 }
 
-# 1. The Core S3 Bucket Resource
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
   tags   = var.tags
 
-# Enable versioning
+  # Enable versioning
   versioning {
     enabled = true
   }
-# Lifecycle management
+
+  # Lifecycle management
   lifecycle_rule {
     id      = "version-management"
     enabled = true
@@ -30,19 +30,11 @@ resource "aws_s3_bucket" "this" {
     }
   }
 
-  # Block all public access
-  public_access_block_configuration {
-    block_public_acls       = true
-    ignore_public_acls      = true
-    block_public_policy     = true
-    restrict_public_buckets = true
-  }
-  
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"   # Better than AES256
-        kms_master_key_id = "alias/aws/s3" # Default AWS-managed KMS key
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "alias/aws/s3"
       }
     }
   }
